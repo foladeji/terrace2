f7ec28a1e01bca9e3ae59a811b2cb6ad203e5736
foladeji <folade2@illinois.edu>
2021-11-14 23:03:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -649,12 +649,12 @@ this.setupXR();
 
     
     initScene(){
-        this.room = new THREE.LineSegments(
-					new BoxLineGeometry( 6, 6, 6, 6, 6, 6 ),
-					new THREE.LineBasicMaterial( { color: 0x808080 } )
-				);
-        this.room.geometry.translate( 0, 3, 0 );
-        this.scene.add( this.room );
+        // this.room = new THREE.LineSegments(
+		// 			new BoxLineGeometry( 6, 6, 6, 6, 6, 6 ),
+		// 			new THREE.LineBasicMaterial( { color: 0x808080 } )
+		// 		);
+        // this.room.geometry.translate( 0, 3, 0 );
+        // this.scene.add( this.room );
         
         this.createUI();
     }
